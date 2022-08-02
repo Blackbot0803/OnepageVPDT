e8e4cc452e41d2cb6e5eac35224127ec0fd4581d
thientri <thientri.dev@gmail.com>
2022-08-02 16:20:06 +0700

    commit reponsive 01

--- HomePage.js
+++ HomePage.js
@@ -1,8 +1,13 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "50%"
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    if (mediaQuery.matches) {
+      document.getElementById("myNav").style.width = "100%";
+    }
+     else { document.getElementById("myNav").style.width = "50%";}
+    
+    
   }
   
 function closeNav() {
     document.getElementById("myNav").style.width = "0%"
   }
-
