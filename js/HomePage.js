293b5fbcc6d0bb9d28af61bb880a42b4dd8a7d36
thientri <thientri.dev@gmail.com>
2022-08-17 11:08:08 +0700

    add signup form

--- js/HomePage.js
+++ js/HomePage.js
@@ -1,16 +1,16 @@
 function openNav() {
-    const mediaQuery = window.matchMedia('(max-width: 500px)');
-    if (mediaQuery.matches) {
-      document.getElementById("myNav").style.width = "100%";
-    }
-     else { document.getElementById("myNav").style.width = "50%";}
-    
-    
+  const mediaQuery = window.matchMedia('(max-width: 500px)');
+  if (mediaQuery.matches) {
+     document.getElementById("myNav").style.width = "100%";
   }
+  else { document.getElementById("myNav").style.width = "50%";}
+    
+    
+}
   
 function closeNav() {
     document.getElementById("myNav").style.width = "0%"
-  }
+}
 
 function scrollDown() {
   const element = document.getElementById("solution");
---
+++ js/WebsiteTTDN.js
@@ -0,0 +1,14 @@
+function openNav() {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    if (mediaQuery.matches) {
+       document.getElementById("myNav").style.width = "100%";
+    }
+    else { document.getElementById("myNav").style.width = "50%";}
+      
+      
+  }
+    
+  function closeNav() {
+      document.getElementById("myNav").style.width = "0%"
+  }
+  
